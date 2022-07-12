bed75a247896c5ff2208a9c4379bf7566515b21d
Hans Mendoza Alva <83467527+Alaprexian@users.noreply.github.com>
2022-07-12 00:03:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,8 @@
 from datetime import datetime
-from flask import Flask, abort, jsonify, request, render_template
-from flask_cors import CORS
-import flask_sqlalchemy
-from models import setup_db, Carro, Espacio, Registro, Pago
-
-A=Espacio()
-B=Espacio()
-C=Espacio()
-D=Espacio()
-F=Espacio()
-G=Espacio()
-H=Espacio()
-I=Espacio()
-J=Espacio()
-K=Espacio()
+from flask import Flask, abort, jsonify, request, make_response
+from flask_cors import CORS, cross_origin
+from flask_jwt_extended import create_access_token, current_user, jwt_required, get_jwt_identity
+from models import Usuario, setup_db, Carro, Espacio, Registro, Pago
 
 TODOS_PER_PAGE=5
 
@@ -26,13 +15,11 @@ def paginate(request, seleccion):
     current_todos = todos[start:end]
     return current_todos
 
-def autenticate():
-    return
-
 def create_app(test_config=None):
-    app = Flask(__name__, template_folder='estacionamiento')
+    app = Flask(__name__)
     setup_db(app)
-    CORS(app, origins=['http://192.168.1.16:8080/'], max_age=10)    
+    CORS(app, supports_credentials=True)
+    cors = CORS(app, resources={r"http://localhost:8081/*": {"origins": "*"}})    
 
     @app.after_request
     def after_request(response):
@@ -40,11 +27,26 @@ def create_app(test_config=None):
         response.headers.add('Access-Control-Allow-Methods', 'GET, OPTIONS, POST, PATCH, DELETE')
         return response
 
-    @app.route('/')
-    def index():
-        return render_template('index.html')
-
+    @app.route('/login', methods=['POST'])
+    def login():
+        data = request.get_json()
+        username = data['username']
+        password = data['password']
+        user = Usuario.query.filter_by(username=username, password=password).first()
+        if user is None:
+            return jsonify({
+                'success': False,
+                'message': 'User not found'
+                 })
+        access_token = create_access_token(identity=user.id)
+        return jsonify({
+            'success': True,
+            'token': access_token,
+            'message': 'Logged in successfully'
+        })
+    
     @app.route('/carros', methods=['GET'])
+    @jwt_required()
     def get_carro():
         selection=Carro.query.order_by('id').all()
         carros=paginate(request, selection)
@@ -63,12 +65,14 @@ def create_app(test_config=None):
                 abort(500)
         placa=body.get('placa', None)
         id_espacio=body.get('id_espacio', None)
-        date=datetime.datetime.now()
+        date=datetime.now()
         espacio=Espacio.query.filter(Espacio.id==id_espacio)
+        '''
         if espacio.libre==False: 
             abort(500)
         espacio.libre=False
         espacio.update()
+        '''  
         try:
             carro=Carro(date=date, placa=placa, id_espacio=id_espacio)
             new_carro=carro.insert()
@@ -129,17 +133,88 @@ def create_app(test_config=None):
 
     
     @app.route('/pagos', methods=['GET'])
-    def get_pago():
-        selection=Pago.query.order_by('id').all()
-        pagos=paginate(request, selection)
-        if len(pagos)==0:
-            abort(404)
+    @jwt_required()
+    def get_pagos():
+        pagos = Pago.query.all()
+        current_pagos = paginate(request, pagos)
         return jsonify({
-            'success':True,
-            'pagos':pagos,
-            'total_pagos':len(pagos)
+            'success': True,
+            'pagos': current_pagos,
+            'total_pagos': len(pagos)
         })
     
+    @app.route('/pagos/<int:id>', methods=['GET'])
+    def get_pago(id):
+        pago = Pago.query.get(id)
+        if pago is None:
+            abort(404)
+        return jsonify({
+            'success': True,
+            'pago': pago.format()
+        })
+    
+    @app.route('/pagos', methods=['POST'])
+    def create_pago():
+        body = request.get_json()
+        if not body:
+            abort(400)
+        pago = Pago(
+            id = body['id'],
+            placa = body['placa'],
+            monto = body['monto'],
+        )
+        pago.insert()
+        return jsonify({
+            'success': True,
+            'pago': pago.format()
+        })
+        
+    @app.route('/usuarios', methods=['GET'])
+    def get_usuarios():
+        usuarios = Usuario.query.order_by('id').all()
+        current_usuarios = paginate(request, usuarios)
+        return jsonify({
+            'success': True,
+            'administradores': current_usuarios,
+            'total_administradores': len(usuarios)
+        })
+    
+    @app.route('/usuarios/<int:id>', methods=['GET'])
+    def get_usuario(id):
+        usuarios = Usuario.query.get(id)
+        if usuarios is None:
+            abort(404)
+        return jsonify({
+            'success': True,
+            'administrador': usuarios.format()
+        })
+    
+    @app.route('/usuarios', methods=['POST'])
+    def create_usuario():
+        body = request.get_json()
+        if not body:
+            abort(400)
+        usuario = Usuario(
+            username = body['username'],
+            password = body['password']
+        )
+        usuario.insert()
+        return jsonify({
+            'success': True,
+            'administrador': usuario.format()
+        })
+        
+    @app.route('/usuarios/<int:id>', methods=['DELETE'])
+    def delete_usuario(id):
+        usuario=Usuario.query.get(id).one_or_none()
+        if usuario is None:
+            abort(404)
+        usuario.delete()
+        return jsonify({
+            'success':True,
+            'deleted_usuario':id
+        })
+        
     
     @app.errorhandler(404)
     def not_found(error):
@@ -166,3 +241,6 @@ def create_app(test_config=None):
         }), 500
 
     return app
+if __name__ == '__main__':
+    app=create_app()
+    app.run(debug=True)
